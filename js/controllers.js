abb174bb97a22a5859d642b9e2631c771d9545b7
jemble <jeremy.bourgein@gmail.com>
2014-04-13 20:12:13 +0100

    added indicator when adding tasks and groups

--- js/controllers.js
+++ js/controllers.js
@@ -13,11 +13,11 @@ app.controller('NavCtrl',function($scope,$location){
 	console.log($scope.location);
 })
 
-app.controller('TasksCtrl',function($scope,$filter,TaskService,GroupService){
+app.controller('TasksCtrl',function($scope,$filter,$timeout,TaskService,GroupService){
 	$scope.tasks = TaskService.getTasks();
 	$scope.statuses = TaskService.getStatus();
 
-	
+	$scope.isAdded = false;
 	$scope.addTask = function(task){
 		
 		task.date = $filter('date')(task.date, "dd-MMMM-yyyy");
@@ -27,24 +27,38 @@ app.controller('TasksCtrl',function($scope,$filter,TaskService,GroupService){
 		
 		$scope.task = {};
 		$scope.taskForm.$setPristine();
+		swapIsAdded();
+		$timeout(swapIsAdded,3000);
+
 	}
 	
+	function swapIsAdded(){
+		$scope.isAdded = !$scope.isAdded;
+	}
+
 	$scope.removeTask = function(task){
 		TaskService.removeTask(task);
 	}
 
 	$scope.users = GroupService.getUsers();
 })
-app.controller('GroupCtrl',function($scope, GroupService){
+app.controller('GroupCtrl',function($scope,$timeout, GroupService){
 	$scope.users = GroupService.getUsers();
+	$scope.isAdded = false;
 
 	$scope.addUser = function(user){
 		GroupService.addUser(user);
 		$scope.newUser = '';
+		swapIsAdded();
+		$timeout(swapIsAdded,3000);
 	}
 
 	$scope.removeUser = function(user){
 		GroupService.removeUser(user);
+	}
+
+	function swapIsAdded(){
+		$scope.isAdded = !$scope.isAdded;
 	}
 
 })
